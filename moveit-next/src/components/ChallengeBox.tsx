387119879585c606b6fe09b309215d74890a4b51
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-05-05 17:02:36 -0300

    fix(logic): Challenge buttons was not reseting the countdown component properly and leveling components was not updating when challenges were completed

--- moveit-next/src/components/ChallengeBox.tsx
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -1,10 +1,23 @@
 import { useContext } from "react"
 import { ChallengesContext } from "../contexts/ChallengesContext"
+import { CountdownContext } from "../contexts/CountdownContext"
 
 import styles from "../styles/components/ChallengeBox.module.css"
+import { CompleteChallenges } from "./CompletedChallenges"
 
 export function ChallengeBox() {
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext)
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext)
+    const {  resetCountdown } = useContext(CountdownContext)
+
+    function handleChallengeFailed() {
+        resetChallenge()
+        resetCountdown()
+    }
+
+    function handleChallengeSucceeded() {
+        completeChallenge()
+        resetCountdown()
+    }
 
     return (
         <div className={ styles.challengeBoxContainer }>
@@ -17,8 +30,8 @@ export function ChallengeBox() {
                         <p>{activeChallenge.description}</p>
                     </main>
                     <footer>
-                        <button type="button" className={ styles.challengeFailedButton } onClick={resetChallenge}>Falhei</button>
-                        <button type="button" className={ styles.challengeSucceededButton }>Completei</button>
+                        <button type="button" className={ styles.challengeFailedButton } onClick={handleChallengeFailed}>Falhei</button>
+                        <button type="button" className={ styles.challengeSucceededButton } onClick={handleChallengeSucceeded}>Completei</button>
                     </footer>
                 </div>
             ) : (
--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { ChallengesProvider } from "../contexts/ChallengesContext"
+import { CountdownProvider } from "../contexts/CountdownContext"
 
 import "../styles/global.css"
 
