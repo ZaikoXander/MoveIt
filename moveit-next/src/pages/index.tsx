b4ad496a1a344622180549754e1c88b68b66d2b7
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-05-06 00:48:53 -0300

    feat(storage): added storage of the data via cookies

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { GetServerSideProps } from 'next'
 
 import { ExperienceBar } from "../components/ExperienceBar"
 import { Profile } from "../components/Profile"
@@ -7,27 +8,54 @@ import { Countdown } from "../components/Countdown"
 import { ChallengeBox } from "../components/ChallengeBox"
 
 import styles from "../styles/pages/Home.module.css"
+import { ChallengesProvider } from "../contexts/ChallengesContext"
 import { CountdownProvider } from "../contexts/CountdownContext"
 
-export default function Home() {
+interface HomeProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={ styles.container }>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompleteChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+    <ChallengesProvider level={props.level} currentExperience={props.currentExperience} challengesCompleted={props.challengesCompleted}>
+      <div className={ styles.container }>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompleteChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
 }
+
+// Post (Título, descrição)
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted} = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
+}
+
+// Back-end (Ruby)
+// Next.js (Node.js)
+// Front-end (React)
