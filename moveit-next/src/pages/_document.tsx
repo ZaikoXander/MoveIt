086d60260db74141d50c69a9a41d70721c75ca96
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-07-02 19:26:55 -0300

    fix(next-version): google fonts importing

--- moveit-next/src/pages/_document.tsx
+++ moveit-next/src/pages/_document.tsx
@@ -1,20 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang='pt-br'>
-                <Head>
-                    <link rel="shortcut icon" href="favicon.png" type="image/png" />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang='pt-br'>
+        <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
